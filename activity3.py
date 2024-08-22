a9608d3bf459bd52d74191fa9294f6828e12208c
mustafa276 <mmustafamajeed1@gmail.com>
2024-08-22 12:29:12 +0500

    if else block

---
+++ activity3.py
@@ -0,0 +1,8 @@
+i = int(input("enter a random integer"))
+if (i < 15):
+    print("i is smaller than 15")
+    print("i'm in the if block")
+else:
+      print("i is greater than 15")
+      print("i'm in the else block")
+print("i'm in neither")
